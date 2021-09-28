a81769c933c3e9e96ed6b1ced28f4026ec077a89
MOHD_VAIS <mohammadvais2@gmail.com>
2021-09-28 10:01:01 +0530

    All files

---
+++ src/components/TextForm.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+
+
+
+export default function TextForm(props) {
+    const handleUpclick = ()=>{
+        // console.log("Uppercase was clicked:" + text);
+        // setText("You have clicked on handleUpclick")
+        let newText =text.toUpperCase();
+        setText(newText)
+
+    }
+
+    const handleLoclick = ()=>{
+        // console.log("Uppercase was clicked:" + text);
+        // setText("You have clicked on handleUpclick")
+        let newText =text.tolowerCase();
+        setText(newText)
+
+    }
+
+    const handleOnChange =(event)=>{
+        // console.log("On change");
+        setText(event.target.value);
+    }
+    const [text, setText] = useState("Enter text here");
+    // text= "new text"; //Wrong way to change the state
+    // setTeext("new text"); //Correct way to change the state
+    return(
+        <div>
+            <div className="mb-3">
+                <h1>{props.heading}</h1>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to upercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLoclick}>Convert to lowercase</button>
+
+            <div className="container my-3">
+                <h3>This is your text Summary</h3>
+                <p> {text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minuts read</p>
+                <h2>Priview</h2>
+                <p>{text}</p>
+            </div>
+        </div>
+        
+    );
+}
